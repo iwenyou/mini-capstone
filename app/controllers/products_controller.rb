62b91c2b2afac4164d73e3fb3ed526c9e83647a7
Wen Zhang <iwenyou@gmail.com>
2017-01-23 18:12:36 -0800

    routine update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,16 +2,30 @@ class ProductsController < ApplicationController
 
   def index
 
-    @nike = Product.first
+    @top_left = Product.first
 
-    @adidas = Product.second
+    @top_middle = Product.second
 
-    @nb = Product.third
+    @top_right = Product.third
 
 
 
     render "index.html.erb"
   end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    product = Product.create(
+    name: params[:name],
+    price: params[:price],
+    description: params[:description])
+    render "create.html.erb"
+  end
+
+
 
   def show
     product_id = params[:id]
@@ -20,4 +34,36 @@ class ProductsController < ApplicationController
 
     render 'show.html.erb'
   end
+
+  def edit
+    product_id = params[:id]
+
+    @product = Product.find_by(id: product_id)
+
+    render "edit.html.erb"
+  end
+
+  def update
+    product_id = params[:id]
+
+    @product = Product.find_by(id: product_id)
+
+    @product.name = params[:name]
+
+    @product.save
+
+    render "update.html.erb"
+
+  end
+
+  def destory
+    product_id = params[:id]
+
+    @product = Product.find_by(id: product_id)
+
+    @product.destroy
+
+    render "destory.html.erb"
+  end
+
 end
